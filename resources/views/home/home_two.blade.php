e1c6caba3b0a5367dd09778cf3bc0e359dfa033f
MareaRock <lucas.greco129@gmail.com>
2021-08-09 11:46:33 -0300

    se cambia col-md-5 por 6.

--- resources/views/home/home_two.blade.php
+++ resources/views/home/home_two.blade.php
@@ -819,7 +819,7 @@
                     </div>
                     <div class="row d-flex justify-content-around">
                     
-                        <div class="discovery-tiles col-sm-12 col-md-5" >
+                        <div class="discovery-tiles col-sm-12 col-md-6" >
                             
                                 <div class=" shadow mb-5 bg-white rounded">
                                     <div>
@@ -841,7 +841,7 @@
                             
                         </div>
 
-                        <div class="discovery-tiles col-sm-12 col-md-5" >
+                        <div class="discovery-tiles col-sm-12 col-md-6" >
                             
                                 <div class="shadow mb-5 bg-white rounded">
                                     <div>
@@ -863,7 +863,7 @@
                             
                         </div>
 
-                        <div class="discovery-tiles col-sm-12 col-md-5" >
+                        <div class="discovery-tiles col-sm-12 col-md-6" >
                             
                                 <div class="shadow mb-5 bg-white rounded">
                                     <div>
@@ -885,7 +885,7 @@
                             
                         </div>
 
-                        <div class="discovery-tiles col-sm-12 col-md-5" >
+                        <div class="discovery-tiles col-sm-12 col-md-6" >
                             
                                 <div class="shadow mb-5 bg-white rounded">
                                     <div>
