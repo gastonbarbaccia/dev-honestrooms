c81795537b7c195f54eca00e0f9132c6545a20e6
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-08-18 13:05:19 -0300

    Se crea un css personalizado llamado custom.css

--- resources/views/home/home_two.blade.php
+++ resources/views/home/home_two.blade.php
@@ -393,9 +393,7 @@
                 </div>
                 <!-------------------------------- Fin de muestra de habitaciones ------------------------->
                  <!------------------------------ Nuestro diferencial INICIO ----------------------------->
-                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-                    crossorigin="anonymous">
+                 <link href="http://honestrooms-qa.herokuapp.com/public/css/custom.css" rel="stylesheet">
 
                     <div class="discovery-section explore_community exploer_banner page-container-no-padding"
                     id="discover-recommendations" ng-init="city_count=city_count-1 ;">
