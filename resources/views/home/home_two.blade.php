ad6c2d02e0d9bc03b96cc4a721bb937736ca481f
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-08-18 12:30:02 -0300

    se quita css para prueba de politicas

--- resources/views/home/home_two.blade.php
+++ resources/views/home/home_two.blade.php
@@ -393,9 +393,9 @@
                 </div>
                 <!-------------------------------- Fin de muestra de habitaciones ------------------------->
                  <!------------------------------ Nuestro diferencial INICIO ----------------------------->
-                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+               <!--   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
                     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-                    crossorigin="anonymous">
+                    crossorigin="anonymous"> -->
 
                     <div class="discovery-section explore_community exploer_banner page-container-no-padding"
                     id="discover-recommendations" ng-init="city_count=city_count-1 ;">
