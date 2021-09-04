84dcf04312eb2ec56f522fbbf3c819e11d368c68
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-09-04 14:03:53 -0300

    Se mejora la division de los textos y las imagenes para que puedan quedar bien

--- resources/views/home/home_two.blade.php
+++ resources/views/home/home_two.blade.php
@@ -1,28 +1,33 @@
 @extends('template_two')
 
 @section('main')
-<main>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Spartan:wght@200;400;600;800&display=swap');
+    <main>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Spartan:wght@200;400;600;800&display=swap');
 
-        * {
-            font-family: 'Lato', sans-serif;
-
-
-        }
-    </style>
+            * {
+                font-family: 'Lato', sans-serif;
 
 
-    <div class="whole-section sect_home" ng-controller="home_owl" ng-cloak>
-        <div class="page-container-responsive new-page-container mini-rel-top row-space-top-1" style="margin-bottom: 5%;">
-            <!------------------------------ Banner principal ------------------------------------->
-            <div class="panel">
-                <div id="discovery-container" class="pad-sm-20 mt-5" style="float:left;width:100%;/* height: 100% !important; background-image: url('http://honestrooms.herokuapp.com/images/home/home_two_new.jpg') ;*/height:200px ;width: 1500px;margin-left: -170px;padding-right: 100px; padding-left: 20%;background-repeat: no-repeat;">
-                <img src="http://honestrooms-qa.herokuapp.com/images/home/banner.jpg" alt="" class="my-5 mt-md-0" style="position: absolute; left: 0; width: 100%; height: 400px; object-fit: cover;margin-top: -200px !important;">
-                    <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12" style="margin-top:5%">
-                        <div class="no-mar-sm" style="margin-top: 94px;">
-                            <div class="textHeaderContainerMarginTop_13o8qr2-o_O-textHeaderContainerWidth_peyti4 row-space-7">
-                                {{-- <h1 class="textHeader_8yxs9w" style="color:white !important;">
+            }
+
+        </style>
+
+
+        <div class="whole-section sect_home" ng-controller="home_owl" ng-cloak>
+            <div class="page-container-responsive new-page-container mini-rel-top row-space-top-1"
+                style="margin-bottom: 5%;">
+                <!------------------------------ Banner principal ------------------------------------->
+                <div class="panel">
+                    <div id="discovery-container" class="pad-sm-20 mt-5"
+                        style="float:left;width:100%;/* height: 100% !important; background-image: url('http://honestrooms.herokuapp.com/images/home/home_two_new.jpg') ;*/height:200px ;width: 1500px;margin-left: -170px;padding-right: 100px; padding-left: 20%;background-repeat: no-repeat;">
+                        <img src="http://honestrooms-qa.herokuapp.com/images/home/banner.jpg" alt="" class="my-5 mt-md-0"
+                            style="position: absolute; left: 0; width: 100%; height: 400px; object-fit: cover;margin-top: -200px !important;">
+                        <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12" style="margin-top:5%">
+                            <div class="no-mar-sm" style="margin-top: 94px;">
+                                <div
+                                    class="textHeaderContainerMarginTop_13o8qr2-o_O-textHeaderContainerWidth_peyti4 row-space-7">
+                                    {{-- <h1 class="textHeader_8yxs9w" style="color:white !important;">
                                     <span class="textHeaderTitle_153t78d-o_O-textHeader_rausch_hp6jb4" style="color:white !important;">
                                         <!--{{ $site_name }}--> There is a room that awaits you.
                                     </span>
@@ -33,107 +38,126 @@
                                     </div>
                                     <!-- /react-text -->
                                 </h1> --}}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!------------------------------ Fin Banner principal ------------------------------------->
+                <!------------------------------ Fin Banner principal ------------------------------------->
 
 
-            {{--HostExperienceBladeCommentStart
+                {{-- HostExperienceBladeCommentStart
               <div ng-repeat="featured_category in featured_host_experience_categories">
                 <div class="lazy-load-div1 lazy-load1 col-md-12 col-lg-12 col-sm-12 col-xs-12  p-0" id="lazy_load_slider" ng-if="featured_category.all_host_experiences_count > 0">
                 @include('host_experiences.home_category_slider')
                 </div>
               </div>
-            HostExperienceBladeCommentEnd--}}
-            <div class="discovery-section explore_community exploer_banner page-container-no-padding mt-0 mt-md-5" id="discover-recommendations" ng-init="city_count=city_count-1 ;">
-                <div class="section-intro text-center row-space-6 row-space-top-8" ng-if="home_city_explore.length!=0">
-                    <h2 class="row-space-1">
-                        <!-- <strong>{{ trans('messages.home.explore_world') }}</strong>-->
-                        ¿Cómo funciona?
-                    </h2>
-                    <p class="text-lead" style="text-align: center;padding-left: 20%;padding-right: 20%;">
-                       <!--  {{ trans('messages.home.explore_desc') }} -->
+            HostExperienceBladeCommentEnd --}}
+                <div class="discovery-section explore_community exploer_banner page-container-no-padding mt-0 mt-md-5"
+                    id="discover-recommendations" ng-init="city_count=city_count-1 ;">
+                    <div class="section-intro text-center row-space-6 row-space-top-8" ng-if="home_city_explore.length!=0">
+                        <h2 class="row-space-1">
+                            <!-- <strong>{{ trans('messages.home.explore_world') }}</strong>-->
+                            ¿Cómo funciona?
+                        </h2>
+                        <p class="text-lead" style="text-align: center;padding-left: 20%;padding-right: 20%;">
+                            <!--  {{ trans('messages.home.explore_desc') }} -->
 
-                       Omnis nostrum totam temporibus harum optio quia molestias iste itaque.<br>
-                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                       
-                    </p>
-                </div>
+                            Omnis nostrum totam temporibus harum optio quia molestias iste itaque.<br>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
 
-                <div class="discovery-tiles">
-                    <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center">
-                            <div>
-                                <img src="http://honestrooms.herokuapp.com/images/home/1.png">
-                            </div>
-                            <div>
-                                <h4 style="color: #24A4BA ; font-weight: 700;font-size: 140%;">Buscá y elegí con seguridad</h4>
-                            </div>
-                            <div style="padding-left:5%;padding-right:5%">
-                                <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
-                                    Sabemos que lo primero antes de tomar una decision es informarse, por eso te acercamos descripciones detalladas tanto de la propiedad como del barrio.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center">
-                            <div>
-                                <img src="http://honestrooms.herokuapp.com/images/home/2.png">
-                            </div>
-                            <div>
-                                <h4 style="color: #24A4BA ; font-weight: 700;font-size: 140%;">Reservá online</h4>
-                            </div>
-                            <div style="padding-left:5%;padding-right:5%">
-                                <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
-                                    Una vez que hagas la consulta de reserva, el propietario tendrá hasta 24hs para confirmar la solicitud. Podran hablar a través de nuestra plataforma para despejar dudas.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center">
-                            <div>
-                                <img src="http://honestrooms.herokuapp.com/images/home/3.png">
-                            </div>
-                            <div>
-                                <h4 style="color: #24A4BA; font-weight: 700;font-size: 140%;">Confirmación y pago</h4>
-                            </div>
-                            <div style="padding-left:5%;padding-right:5%">
-                                <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
-                                    Ni bien el propietario acepte tu reserva podrás realizar el pago a través de Mercado Pago o Paypal y te pondremos en contacto con tu anfitrión para coordinar tu mudanza.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center">
-                            <div>
-                                <img src="http://honestrooms.herokuapp.com/images/home/4.png">
-                            </div>
-                            <div>
-                                <h4 style="color: #24A4BA; font-weight: 700;font-size: 140%;">Todo listo para mudarte</h4>
-                            </div>
-                            <div style="padding-left:5%;padding-right:5%">
-                                <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
-                                    ¡Llegó el dia! Ya tenes un nuevo hogar y llegaste al comienzo de tu estadía en esta hermosa ciudad. Te recibirá tu anfitrión y realizarán la firma online del contrato.
-                                </p>
-                            </div>
-                        </div>
+                        </p>
+                    </div>
 
+                    <div class="discovery-tiles">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center;width: 100%;">
+                                <div style="width: 40%">
+                                    <img src="http://honestrooms.herokuapp.com/images/home/1.png">
+                                </div>
+                                <div>
+                                    <div>
+                                        <h4 style="color: #24A4BA ; font-weight: 700;font-size: 140%;">Buscá y elegí con
+                                            seguridad</h4>
+                                    </div>
+                                    <div style="padding-left:5%;padding-right:5%">
+                                        <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
+                                            Sabemos que lo primero antes de tomar una decision es informarse, por eso te
+                                            acercamos descripciones detalladas tanto de la propiedad como del barrio.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center;width: 100%;">
+                                <div style="width: 40%">
+                                    <img src="http://honestrooms.herokuapp.com/images/home/2.png">
+                                </div>
+                                <div>
+                                    <div>
+                                        <h4 style="color: #24A4BA ; font-weight: 700;font-size: 140%;">Reservá online</h4>
+                                    </div>
+                                    <div style="padding-left:5%;padding-right:5%">
+                                        <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
+                                            Una vez que hagas la consulta de reserva, el propietario tendrá hasta 24hs para
+                                            confirmar la solicitud. Podran hablar a través de nuestra plataforma para
+                                            despejar
+                                            dudas.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center;width: 100%;">
+                                <div style="width: 40%">
+                                    <img src="http://honestrooms.herokuapp.com/images/home/3.png">
+                                </div>
+                                <div>
+                                    <h4 style="color: #24A4BA; font-weight: 700;font-size: 140%;">Confirmación y pago</h4>
+                                </div>
+                                <div style="padding-left:5%;padding-right:5%">
+                                    <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
+                                        Ni bien el propietario acepte tu reserva podrás realizar el pago a través de Mercado
+                                        Pago o Paypal y te pondremos en contacto con tu anfitrión para coordinar tu mudanza.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-3" style="float:left;text-align:center;width: 100%;">
+                                <div style="width: 40%">
+                                    <img src="http://honestrooms.herokuapp.com/images/home/4.png">
+                                </div>
+                                <div>
+                                    <div>
+                                        <h4 style="color: #24A4BA; font-weight: 700;font-size: 140%;">Todo listo para
+                                            mudarte
+                                        </h4>
+                                    </div>
+                                    <div style="padding-left:5%;padding-right:5%">
+                                        <p class="content-font" style="margin-left: 5%;margin-right: 5%;font-size:16px">
+                                            ¡Llegó el dia! Ya tenes un nuevo hogar y llegaste al comienzo de tu estadía en
+                                            esta
+                                            hermosa ciudad. Te recibirá tu anfitrión y realizarán la firma online del
+                                            contrato.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+
+                <div style="text-align:center;margin-top:4%;font-size:20px">
+                    <a href="" style="color:#24A4BA;text-decoration: none;">QUIERO SABER MÁS <i class="fa fa-angle-right"
+                            style="color:#24A4BA;padding-left:3px;"></i></a>
+                </div>
+
+
             </div>
 
-            <div style="text-align:center;margin-top:4%;font-size:20px">
-                <a href="" style="color:#24A4BA;text-decoration: none;">QUIERO SABER MÁS <i class="fa fa-angle-right" style="color:#24A4BA;padding-left:3px;"></i></a>
-            </div>
+            <!----------------------------- Inicio de Footer ------------------------->
 
+            <!--------------------- Fin de Footer ----------------------------->
+        </div>
+        </div>
 
-    </div>
-
-    <!----------------------------- Inicio de Footer ------------------------->
-
-    <!--------------------- Fin de Footer ----------------------------->
-    </div>
-    </div>
-
-</main>
+    </main>
 @stop
