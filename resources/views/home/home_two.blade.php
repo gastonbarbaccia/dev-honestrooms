d800a58639091b1bf6bb2ed4b618907b303c1479
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-09-04 14:35:52 -0300

    Se agrega margen del como funciona

--- resources/views/home/home_two.blade.php
+++ resources/views/home/home_two.blade.php
@@ -55,7 +55,7 @@
             HostExperienceBladeCommentEnd --}}
                 <div class="discovery-section explore_community exploer_banner page-container-no-padding mt-0 mt-md-5"
                     id="discover-recommendations" ng-init="city_count=city_count-1 ;">
-                    <div class="section-intro text-center row-space-6 row-space-top-8" ng-if="home_city_explore.length!=0">
+                    <div class="section-intro text-center row-space-6 row-space-top-8" ng-if="home_city_explore.length!=0" style="margin-top: 10%;">
                         <h2 class="row-space-1">
                             <!-- <strong>{{ trans('messages.home.explore_world') }}</strong>-->
                             ¿Cómo funciona?
