92e585b7ba51584fb187b7bf6bd0dcd4bd6c1b10
MareaRock <lucas.greco129@gmail.com>
2021-09-27 13:03:48 -0300

    se agrega text-center

--- resources/views/home/how_it_works.blade.php
+++ resources/views/home/how_it_works.blade.php
@@ -83,10 +83,10 @@
                 </p>
             </div>
 
-            <div class="discovery-tiles">
+            <div class="discovery-tiles text-center">
                 <div class="row">
                 <div class="container">
-                        <div class="row" style="margin-right: 0px; margin-left: 0px">
+                        <div class="row" style="margin-right: 20px; margin-left: 20px">
                             <div class="col-12 mb-4" > <!--1er item-->
                                 <div class="row">
                                     <div class="col-md-4 col-lg-3 text-center">
